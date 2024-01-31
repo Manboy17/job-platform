32d0b74513797848b3146e728b2b6f8ef5276ebd
denys <denia15042005@gmail.com>
2024-01-31 18:12:39 +0100

    display created vacancies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import Filters from "@/components/shared/Filters";
+import JobCard from "@/components/shared/JobCard";
+import ProductDetails from "@/components/shared/ProductDetails";
 import Search from "@/components/shared/Search";
+import { getJobs } from "@/lib/actions/job.action";
+import { IJob } from "@/lib/database/models/job.model";
 import React from "react";
 
-const MainPage = () => {
+const MainPage = async () => {
+  const jobs: IJob[] = await getJobs();
   return (
     <>
       <section className="bg-blue-600 w-full py-4">
@@ -20,6 +25,24 @@ const MainPage = () => {
         <div className="flex flex-col items-center w-full justify-between gap-6 lg:flex-row">
           <Search />
           <Filters />
+        </div>
+      </section>
+
+      <section className="bg-gray-100">
+        <div className="wrapper py-5 flex">
+          <div className="w-1/3">
+            <span className="text-xs text-gray-500">
+              {jobs.length} Job Posting Available
+            </span>
+            <div className="flex flex-col gap-5 mt-3">
+              {jobs.map((job: IJob) => (
+                <JobCard key={job._id} job={job} />
+              ))}
+            </div>
+          </div>
+          <div className="w-2/3">
+            <ProductDetails />
+          </div>
         </div>
       </section>
     </>
